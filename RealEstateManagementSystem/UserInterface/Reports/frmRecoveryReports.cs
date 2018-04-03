6c131816ba8d2457593345883f83e10d9b695b0c
Asif Chowdhury <asifchowdhury@outlook.com>
2018-04-03 16:26:07 -0400

    Test Commit with Recovery Report form

---
+++ RealEstateManagementSystem/UserInterface/Reports/frmRecoveryReports.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RealEstateManagementSystem.UserInterface.Reports
+{
+    public partial class frmRecoveryReports : Form
+    {
+        public frmRecoveryReports()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+        }
+
+        private void frmRecoveryReports_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
